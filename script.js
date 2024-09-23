dc9df8c4e60b99c0861dfb2790981fc1db9992d4
Egor <cefranovegor@gmail.com>
2024-09-23 20:25:52 +0300

    добавленые изминения

---
+++ script.js
@@ -0,0 +1,62 @@
+const userInput1 = +prompt("Введите число")
+const userInput2 = +prompt("Введите ещё число")
+
+
+//function isPositive(number){  
+  //  if (number > 0){
+ //       return "Положительное"
+   // } else if (number < 0) {
+  //       return "Отрицательное"
+  //  } else if (nimber === 0){
+   //     return "Ноль"
+  //  } else {
+  //      return "Введеное число некорректно"
+  //  }
+//}
+
+//function isEarth(planet){
+  // planet - planet.toLowerCase()
+
+    //return planet === 'земля' ?
+   // 'Привет землянин!' :
+   // 'Привет,инопланетянин'
+// }
+
+//function pow(num, p) {
+   // let result = 1
+    //while (p){
+    //    p--
+    //    result = result * num
+  //  }
+   // return result
+ //}
+
+ //function calculate(num1,num2,operator){
+    //switch(operator){
+       // case '+':
+       //     return num1+num2
+      //  case '-':
+       //     return num1 - num2
+      //  case '*':
+      //      return num1 * num2
+       // case '/':
+      //      return num1 / num2
+       // default:
+       //     return "Будьте внимательнее!"
+   // }
+ //}
+
+ //function mod(num1,num2){
+   // if (num <  num2){
+   //     return num1
+    //} else if (num1 === num2){
+    // return 0
+   // }else {
+       // let result = num2
+        //while (result <= num1){
+       //   result += num2
+       // }
+     // return num1 - (result - num2)
+  // }
+// }
+alert(isKratno(userInput1, userInput2))
---
+++ script2.js
@@ -0,0 +1,14 @@
+const userInput = +prompt("Сколько вам лет?")
+
+
+ //function getModule(number){
+ // if(number >=0) {
+  //  return number
+ // }else if (number< 0) {
+ //   return -number
+ //} else {
+   // return "Некорректный ввод"
+ //}
+  
+//}
+alert(getModule(userInput))
